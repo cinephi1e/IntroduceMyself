735db20e0105494b42a8f120906baff7c1d5acca
cinephile <aprilkb412@naver.com>
2022-11-02 20:32:49 +0900

    방명록 역순 정렬

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ def home():
 @app.route("/yun/guestbook", methods=["POST"])
 def introduction_post():
     name_receive = request.form["name_give"]
-    comment_receive = request.form["comment_give"]
+    guestComment_receive = request.form["guestComment_give"]
     date_receive = request.form["date_give"]
     ## 추가
     guestbookList = list(db.bsy.find({}, {'_id': False}))
     count = len(guestbookList) + 1
     doc = {
         'name': name_receive,
-        'comment': comment_receive,
+        'comment': guestComment_receive,
         'date': date_receive,
         'num': count,
         'read': 0
@@ -36,23 +36,11 @@ def introduction_post():
 ## 불러오기
 @app.route("/yun/guestbook", methods=["GET"])
 def introduction_get():
-    comment_list = list(db.bsy.find({},{'_id':False}))
-    return jsonify({'comments':comment_list})
+    guestComment_list = list(db.bsy.find({},{'_id':False}))
+    return jsonify({'guestComments':guestComment_list})
 
 
-
-## 삭제하기1
-@app.route("/yun/guestbook", methods=["POST"])
-def introduction_delete():
-    id_receive = request.form["id_give"]
-    doc = { 'id': id_receive }
-
-    db.bsy.delete_one(doc)
-
-    return jsonify({'msg': '삭제 완료 ✅'})    
-
-
-## 삭제하기2
+## 삭제하기
 @app.route("/yun/guestbook/remove", methods=["POST"])
 def introduction_remove():
     id_receive = request.form["id_give"]
@@ -70,6 +58,7 @@ def introduction_read():
     num_receive = request.form["num_give"]
     db.bsy.update_one({'num': int(num_receive)}, {'$set': {'read': 1}})
     return jsonify({'msg': '방명록 확인 ✅'})
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
